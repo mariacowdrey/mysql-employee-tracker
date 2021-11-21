2ef938d6c19f78840a24feb2f606a533f9bb4105
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-11-20 20:49:04 -0800

    update sql seeds and schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,28 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
-USE EMPLOYEE_DB;
+DROP DATABASE IF EXISTS employeesDB;
 
-CREATE TABLE employee (
-	id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL, 
-    role_id INT NOT NULL,
-    PRIMARY KEY(id)
-);
+CREATE DATABASE employeesDB;
 
-CREATE TABLE employee_role (
-	id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10, 0) NOT NULL,
-    department_id int NOT NULL,
-    PRIMARY KEY(id)
-);
+USE employeesDB;
 
 CREATE TABLE department (
-	id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY(id)
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NULL,
+  PRIMARY KEY (id)
 );
 
-SELECT Math FROM department;
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(45) NULL,
+  salary DECIMAL(10.3) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(45) NULL,
+  last_name VARCHAR(45) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,38 @@
-USE employee_db;
+USE employeesDB;
 
-INSERT INTO department (name) VALUES ("Human Resources");
-INSERT INTO department (name) VALUES ("Marketing");
-INSERT INTO department (name) VALUES ("Information Technology");
-INSERT INTO department (name) VALUES ("Corporate");
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("Engineering");
+INSERT INTO department (name)
+VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Legal");
 
-INSERT INTO role (title, salary, department_id) VALUES ("Analyst", 80, 3);
-INSERT INTO role (title, salary, department_id) VALUES ("Software Engineer", 70, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Social Media Manager", 50, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Director", 100, 1);
-INSERT INTO role (title, salary, department_id) VALUES ("Director", 100, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 250000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Jacob", "Black", 2);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Hazel", "Giles", 1);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Andrew", "Lim", 3);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Jane", "Elliott", 4);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Matt", "Sparks", 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jane", "Doe", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Norah", "Rios", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kate", "Mellor", 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Shiv", "Betts", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Leen", "Hurst", 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Gary", "Leer", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tom", "Cruise", 4, 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Daisy", "Hooper", 1, 2);
